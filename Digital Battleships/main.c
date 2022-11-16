d28572bf05360f77b25f22f4329261706f948f44
KostyaBIST221 <ksapov24@mail.ru>
2022-11-16 19:26:25 +0300

    Initial Commit

---
+++ Digital Battleships/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Digital Battleships
+//
+//  Created by kostya on 16.11.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
